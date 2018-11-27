49d41994360460e19c08f4ba2997d21e5b4f6767
Abi Raskin <abiraskin@gmail.com>
2018-11-27 04:31:29 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,10 @@
 class BankAccount
 
+  attr_accessor :name :account
+
+  def intialize(name, account)
+    @name = name
+    @account = []
+    account.balance = "1000"
+    account.status = "open"
 end
