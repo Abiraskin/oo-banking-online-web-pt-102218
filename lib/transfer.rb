e44e05994379313feff79254cacafc011a3f7cbe
Abi Raskin <abiraskin@gmail.com>
2018-11-28 01:22:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+
+    attr_reader :name
+    attr_accessor :sender, :receiver, :status, :ammount
+
+    def initialize(name)
+      @name = name
+      @sender = sender
+      @receiver = receiver
+      @ammount = ammount
+    end
+      
 end
